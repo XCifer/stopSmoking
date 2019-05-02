efb468d7ae21e2f93177938432150d4578259832
XCifer <13607960126@139.com>
2019-05-02 13:59:29 +0300

    read reply from database

--- readMention.py
+++ readMention.py
@@ -9,7 +9,7 @@ import re
 conn = sqlite3.connect("C:/tweets.db")
 conn.text_factory = bytes
 c = conn.cursor()
-c.execute("SELECT user_id, screen_name, text FROM tweets where text like '%cigarette%';")
+c.execute("SELECT user_id, screen_name, text FROM tweets where text like '%cigarette%' or text like '%smoking%' or text like 'smokes' or text like 'smoked';")
 
 items = c.fetchall()
 print(len(items))
@@ -23,17 +23,16 @@ for i in range(len(items)):
     mentions = []
     text = str(items[i][2])
     mentions = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z0-9_]+)',text)
-    print(mentions)
-    if mentions:
-        for idx in range(len(items)):
-            if items[idx][1] in mentions:
-                G.add_edge(i,idx)
-                print(i,"Edge from ",i," to ",idx," added.")
-    else :
-        print(i)
+    # print(mentions)
+    # if mentions:
+    for idx in range(len(items)):
+        if items[idx][1] in mentions:
+            G.add_edge(i,idx)
+            print(i,"Edge from ",i," to ",idx," added.")
+
 
 
 G.remove_nodes_from(list(nx.isolates(G)))
-nx.draw(G,with_labels=True)
+nx.draw(G,with_labels=True,node_size =100,font_size =10,node_shape='1')
 plt.show()
-plt.savefig('labels.png')
+# plt.savefig('labels.png')
--- readReply.py
+++ readReply.py
@@ -5,39 +5,45 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 
-# read database
-conn = sqlite3.connect("C:/tweets.db")
-conn.text_factory = bytes
-c = conn.cursor()
-c.execute("SELECT user_id, screen_name, text FROM tweets where text like '%cigarette%';")
+def search():
+    # read database
+    # keyword = input("Enter a key word for searching in text: ")
+    conn = sqlite3.connect("C:/tweets.db")
+    conn.text_factory = bytes
+    c = conn.cursor()
+    c.execute("SELECT user_id, screen_name, text, in_reply_to_user_id FROM tweets where text like '%cigarette%' or text like '%smoking%' or text like 'smokes' or text like 'smoked';")
+    items = c.fetchall()
+    print(len(items))
 
-items = c.fetchall()
-print(len(items))
-
-G = nx.DiGraph()
-
-
-for i in range(len(items)):
-    G.add_node(i, screen_name = items[i][0])
-
-for i in range(len(items)):
-    if items[i][1]:
-        for j in range(len(items)):
-            if items[j][0] == items[i][1]:
-                G.add_edge(i,j)
-                print(round(i/len(items),4),"Edge from ",i," to ",j," added.")
-    else :
-        print(round(i/len(items),4))
+    G = nx.DiGraph()
 
 
+    for i in range(len(items)):
+        G.add_node(i, screen_name = items[i][1], user_id=items[i][0])
 
-# G2 = G.copy()
-# inde = G2.in_degree(G2)
-# outde = G2.out_degree(G2)
-# for n in G2.nodes():
-#     if inde[n]<2 and outde[n]<2:
-#         G.remove_node(n)
-G.remove_nodes_from(list(nx.isolates(G)))
-nx.draw(G,with_labels=True)
-plt.show()
-plt.savefig('reply.png')
+    for i in range(len(items)):
+        if items[i][3] != -1:
+            for j in range(len(items)):
+                if items[j][0] == items[i][3]:
+                    G.add_edge(i,j)
+                    print(round(i/len(items),4),"Edge from ",i," to ",j," added.")
+        else :
+            print(round(i/len(items),4))
+    
+    return G
+
+
+   
+
+def draw(G):
+    # G2 = G.copy()
+    # inde = G2.in_degree(G2)
+    # outde = G2.out_degree(G2)
+    # for n in G2.nodes():
+    #     if inde[n]<2 and outde[n]<2:
+    #         G.remove_node(n)
+    G.remove_nodes_from(list(nx.isolates(G)))
+    nx.draw(G,with_labels=True,node_size =100,font_size =10,node_shape='1')
+    plt.show()
+
+draw(search())
